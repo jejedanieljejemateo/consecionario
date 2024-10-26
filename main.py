7cfcfa56dfd178159d9ce5e5490f0c8bb7897672
jejedanieljejemateo <31315598+jejedanieljejemateo@users.noreply.github.com>
2024-10-26 11:51:13 -0500

    cors
    configuracion de cors

--- main.py
+++ main.py
@@ -2,8 +2,36 @@ import uvicorn
 from fastapi import FastAPI
 #from . import crud, models
 from pydantic import BaseModel, PositiveInt
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 post=[]
+
+#creo el modelo
+class Car(BaseModel):
+    name: str
+    brand: str
+    brach: str
+
+# FastAPI app instanceuvicorn app:app
+app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 #creo el modelo
 class Car(BaseModel):
